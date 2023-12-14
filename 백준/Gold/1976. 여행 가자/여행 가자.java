f0850bdf38011de107c2041b0a83005ea2d208b1
s-yeong <102036033+s-yeong@users.noreply.github.com>
2023-12-14 21:41:58 +0900

    [Gold IV] Title: 여행 가자, Time: 136 ms, Memory: 15904 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -10,25 +10,24 @@ import java.util.*;
  * 3. 무방향 그래프
  * [풀이]
  * 해당 지점을 방문체크 할 수 있으면 여행 가능한 경로다.
- * 1. 방문해야 하는 지점을 1로 두고
- * 2. 그 지점을 방문하면 0으로 바꾼다.
- * 3. 시작점에서 연결시킨다.
+ * => 유니온 파인드로 풀어보기
+ * 1. 같은 집합에 속하지 않으면, NO
  */
 public class Main {
 
     static int N, M;
     static int[] plan;
-    static boolean[][] ch;
-    static List<List<Integer>> graph;
+    static int[] parents;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
 
-        graph = new ArrayList<>();
-        for(int i=0;i<=N;i++) {
-            graph.add(new ArrayList<>());
+        //init
+        parents = new int[N+1];
+        for(int idx=1; idx<=N; idx++) {
+            parents[idx] = idx;
         }
 
         for(int i=1; i<=N; i++) {
@@ -36,47 +35,41 @@ public class Main {
             for(int j=1; j<=N; j++) {
                 int isLink = Integer.parseInt(st.nextToken());
                 if(isLink == 1) {
-                    //3. 무방향 그래프
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
+                    union(i, j);
                 }
             }
         }
 
+        String answer = "YES";
         // 여행 계획
-        int startIdx = 0;
-        plan = new int[N+1];
-        ch = new boolean[N+1][N+1];
+        int root = 0;
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<M; i++) {
-            int idx = Integer.parseInt(st.nextToken());
-            if(i==0) startIdx = idx;
 
-            // 방문 해야 하는 지점 = 1
-            plan[idx] = 1;
-        }
+            int num = Integer.parseInt(st.nextToken());
 
-        solve(startIdx);
-
-        String answer = "YES";
-        for(int idx : plan) {
-            // 방문안한 지점이 있으면,
-            if(idx == 1) answer = "NO";
+            if(i==0) root = find(num);
+            else {
+                if(root != find(num)) {
+                    answer = "NO";
+                }
+            }
         }
 
         System.out.println(answer);
     }
-    static void solve(int cur) {
-
-        plan[cur] = 0;
-
-        for(int next : graph.get(cur)) {
-
-            if(ch[cur][next]) continue;
-
-            ch[cur][next] = true;
-            solve(next);
-        }
+    static int find(int a) {
+        if(parents[a] == a) return a;
+        return parents[a] = find(parents[a]);
     }
+    static void union(int a, int b) {
 
+        int fa = find(a);
+        int fb = find(b);
+
+        if(fa == fb) return;
+
+        if(fa < fb) parents[fa] = fb;
+        else parents[fb] = fa;
+    }
 }
