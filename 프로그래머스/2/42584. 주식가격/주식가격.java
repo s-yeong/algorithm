dbcedef21d78df925c24c61c75229605d24ac977
이상영 <102036033+s-yeong@users.noreply.github.com>
2024-07-05 03:17:23 +0900

    [level 2] Title: 주식가격, Time: 24.02 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,66 +5,22 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        PriorityQueue<Price> pQ = new PriorityQueue<>();
-        List<Price> list = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
         
-        pQ.offer(new Price(0, prices[0]));
         for(int idx=1; idx<prices.length; idx++) {
-            
-            Price highprice = pQ.peek(); 
-            int val = prices[idx];
-            
-            // 최대값이 주식 떨어지면,
-            if(highprice.val > val) {
-                while(!pQ.isEmpty()) {
-                    Price price = pQ.poll();
-                    // 떨어졌으면, 시간 기록
-                    if(price.val > val) {
-                        price.time = idx - price.idx;
-                        list.add(price);
-                    }
-                    // 안떨어졌으면,
-                    else {
-                        pQ.offer(price);
-                        break;
-                    }
-                }
+            while(!st.isEmpty() && prices[st.peek()] > prices[idx]) {
+                int stIdx = st.pop();
+                answer[stIdx] = idx - stIdx;
             }
-            // pQ 넣기
-            pQ.offer(new Price(idx, val));
+            st.push(idx);
+        }
+        while(!st.isEmpty()) {
+            int stIdx = st.pop();
+            answer[stIdx] = prices.length - stIdx - 1;
         }
         
-        while(!pQ.isEmpty()) {
-            Price price = pQ.poll();
-            // 안떨어졌으면, 시간추가
-            if(price.time == 0) {
-                price.time = prices.length - price.idx - 1; 
-            }
-            list.add(price);
-        }
-        
-        for(Price price : list) {
-            answer[price.idx] = price.time;
-        }
-            
         
         return answer;
-    }
-    static class Price implements Comparable<Price> {
-        int idx;
-        int val;
-        int time;
-        
-        public Price(int idx, int val) {
-            this.idx=idx;
-            this.val=val;
-            time = 0;
-        }
-        
-        public int compareTo(Price obj) {
-            return obj.val - this.val;
-        }
-        
-        
     }
 }
