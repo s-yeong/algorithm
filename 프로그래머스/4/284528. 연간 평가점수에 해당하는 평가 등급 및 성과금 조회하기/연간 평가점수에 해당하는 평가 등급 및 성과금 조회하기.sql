f2427095fb852f7d29d0bbffd44c972c1c2afb85
이상영 <102036033+s-yeong@users.noreply.github.com>
2024-07-07 01:41:12 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,24 @@
+-- 사원별 성과금 정보를 조회
+-- 평가 점수별 등급과 등급에 따른 성과금 정보
+
+select A.EMP_NO, A.EMP_NAME, B.GRADE,
+(CASE
+ WHEN B.GRADE = 'S' THEN A.SAL * 0.2
+ WHEN B.GRADE = 'A' THEN A.SAL * 0.15
+ WHEN B.GRADE = 'B' THEN A.SAL * 0.1
+ ELSE 0
+ END
+) BONUS
+from HR_EMPLOYEES A
+    JOIN (
+    SELECT EMP_NO, 
+    (CASE
+    WHEN AVG(SCORE) >= 96 THEN 'S'
+    WHEN AVG(SCORE) >= 90 THEN 'A'
+    WHEN AVG(SCORE) >= 80 THEN 'B'
+    ELSE 'C'
+    END) GRADE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+    ) B ON A.EMP_NO = B.EMP_NO
+ORDER BY EMP_NO
